8365ac47ed900329fa0883c2f96fb68ce34e7589
NateSBC <166132358+NateSBC@users.noreply.github.com>
2024-06-18 11:30:29 +0000

    RPS Game

---
+++ rpsgame.py
@@ -0,0 +1,61 @@
+# Make a rock paper scissors app that I can play against the computer. 
+# The program should keep track of the number of rounds played, the number of rounds
+# won by the user, and the number of rounds won by the computer. 
+# After each round, the program should display the results 
+# and ask the user if they want to play another round. 
+# You should use functions, loops, conditionals, random number generation to achieve this.
+
+import random
+
+
+def play_round():
+    choices = ("Rock", "Paper", "Scissors")
+    computer_choice = random.choice(choices)
+    player_choice = input(f"Please enter your choice {choices}: ")
+    print(f"You chose {player_choice} and the computer chose {computer_choice}.")
+    
+    if player_choice == computer_choice:
+            print("Draw")
+    elif player_choice == "Rock" and computer_choice == "Scissors":
+            print("Player Wins!")
+        
+    elif player_choice == "Rock" and computer_choice == "Paper":
+            print("Player Loses!")
+        
+    elif player_choice == "Paper" and computer_choice == "Rock":
+            print("Player Wins!")
+        
+    elif player_choice == "Paper" and computer_choice == "Scissors":
+            print("Player Loses!")
+        
+    elif player_choice == "Scissors" and computer_choice == "Paper":
+            print("Player Wins!")
+        
+    elif player_choice == "Scissors" and computer_choice == "Rock":
+            print("Player Loses!")
+        
+    else:
+            print("Invalid choice. Please enter again.")
+
+    
+        
+
+
+
+num_of_rounds = -1
+computer_wins = 0
+user_wins = 0
+
+while True:
+    num_of_rounds += 1
+    
+
+    print(f"Number of Rounds = {num_of_rounds}\nUser Wins = {user_wins}\nComputer Wins = {computer_wins}")
+    
+    play_again = input("Do you want to play again? (y/n): ")
+    if play_again!= "y":
+        break
+
+    
+    play_round()
+
